0156828bfd36cff9085f7a2c3dc2913ddb378fc7
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-02-06 12:54:27 +0530

    Add new UI components, enhance Navbar, and update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            'avatars.githubusercontent.com', 
+            'lh3.googleusercontent.com', 
+            'res.cloudinary.com',
+            'gomoonbeam.com',
+            'cursor.so',
+            'userogue.com',
+            'editorially.org',
+            'editrix.ai',
+            'app.pixelperfect.quest',
+            'algochurn.com',
+            'ui.aceternity.com',
+            'tailwindmasterkit.com',
+            'smartbridgetech.com',
+            'renderwork.studio',
+            'cremedigital.com',
+            'goldenbellsacademy.com',
+            'invoker.lol',
+            'efreeinvoice.com',
+            'aceternity.com',
+        ],
+    }
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,72 @@
-
+'use client'
 import Navbar from "@/components/global/Navbar";
+import { Button } from "@/components/ui/button";
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
+import { HeroParallax } from "@/components/ui/hero-parallax";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+import { LampContainer } from "@/components/ui/lamp";
+import { clients, products } from "@/lib/constants";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex bg-black min-h-screen flex-col items-center justify-between p-24">
       <Navbar/>
+      <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
 
-   
+        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+        <ContainerScroll
+        titleComponent={
+          <div className="flex items-center flex-col">
+ <Button
+                  size={'lg'}
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                >
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
+                    Start For Free Today
+                  </span>
+                </Button>
+                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Automate Your Work With AutoNode
+                </h1>
+          </div>
+        }
+      >
+        <Image
+          src={`/linear.webp`}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+        </div>
+      </section>
+      <InfiniteMovingCards
+      className="md:mt-[18rem] mt-[-100px]"
+      items={clients.map(client => ({
+        quote: client.href,
+        name: 'Hello',
+        title: 'me'
+      }))}
+      direction="right"
+      speed="slow"
+      />
+          <section >
+      <HeroParallax products={products}></HeroParallax>
+
+          </section>
+          <section className="mt-[-500px]">
+          <LampContainer>
+    
+        Build lamps <br /> the right way
+      
+      
+    </LampContainer>
+          </section>
 
     </main>
   );
