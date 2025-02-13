59a2262306ecbe3947d5e4dfb831e73711a749e6
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-02-13 09:18:53 +0530

    Refactor profile picture component to use next/navigation and add image removal functionality

--- src/app/(main)/(pages)/settings/components/profile-picture.tsx
+++ src/app/(main)/(pages)/settings/components/profile-picture.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import UploadCareButton from './upload-carebutton'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 type Props={
     userImage:string | null
     onDelete ? : any
@@ -9,13 +9,29 @@ type Props={
 
 const ProfilePicture = ({userImage, onDelete,onUpload}:Props) => {
     const router=useRouter()
+
+    const onRemoveProfileImage= async()=>{
+      const response =await onDelete()
+      if(response)
+{
+  router.refresh()
+}    }
     
   return (
     <div className='flex flex=col'>
         <p className='text-lg text-white'>Profile Picture
         </p>
-        <div className='flex h-[30vh]  col items-center'></div>
+        <div className='flex h-[30vh]  col items-center'>
+     {userImage? (
+      <>
+      <div className='relative h-full w-2/12'></div>
+      </>
+
+     ):(
+
       <UploadCareButton onUpload={(e: string) => { console.log(e); }}/>
+     )}
+        </div>
     </div>
   )
 }
