06269eb59b676f07f9e3e077e3044ccd76c0481a
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-02-13 08:54:11 +0530

    Add upload functionality to profile picture component and update props

--- src/app/(main)/(pages)/settings/components/profile-picture.tsx
+++ src/app/(main)/(pages)/settings/components/profile-picture.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import UploadCareButton from './upload-carebutton'
+import { useRouter } from 'next/router'
 type Props={
     userImage:string | null
-    onDelete ? :string |null
+    onDelete ? : any
+    onUpload?: any
 }
 
 const ProfilePicture = ({userImage, onDelete,onUpload}:Props) => {
+    const router=useRouter()
+    
   return (
     <div className='flex flex=col'>
         <p className='text-lg text-white'>Profile Picture
